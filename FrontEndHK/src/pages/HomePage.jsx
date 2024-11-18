201453fc58c57291ddebfc99b164c7968499277d
Jonnathan Saavedra <jonnjstart@gmail.com>
2024-11-18 01:09:28 -0500

    added sword logo in home page

--- FrontEndHK/src/pages/HomePage.jsx
+++ FrontEndHK/src/pages/HomePage.jsx
@@ -1,12 +1,18 @@
 import { Header } from '../components/Header.jsx';
 import { Banner } from '../components/Banner.jsx';
 import { CountDownTimer } from '../components/CountDownTimer.jsx';
+import SwordLogo from "../assets/sword_hack_knight_transparentBG.png";
 
 export function HomePage() {
     return (
         <>
             <Header />
             <div className="min-h-screen bg-gradient-to-br from-blue-100 to-yellow-200 flex flex-col items-center justify-center">
+                <img
+                    src={SwordLogo}
+                    alt="SwordLogo"
+                    className="w-1/3 md:w-1/6 p-2"
+                />
                 <div className="flex flex-col md:flex-row gap-5">
                     <Banner />
                     <CountDownTimer />
