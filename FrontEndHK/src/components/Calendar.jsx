b94dd90869ce87539a3f0ec4e0f6bfd4bee6085d
Jonnathan Saavedra <jonnjstart@gmail.com>
2024-12-12 23:48:50 -0500

    added a calendar to schedule page

---
+++ FrontEndHK/src/components/Calendar.jsx
@@ -0,0 +1,35 @@
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+
+export function Calendar() {
+    return (
+        <div className = "md:m-10">
+            <FullCalendar
+                plugins={[timeGridPlugin]}
+                initialView="timeGrid"
+                hiddenDays={[1, 2, 3, 4]} // Hide Monday-Thursday
+                visibleRange={{
+                    start: "2025-02-07", // Start on Friday (Feb 7, 2025)
+                    end: "2025-02-10",   // End after Sunday (non-inclusive Monday)
+                }}
+                views={{
+                    timeGridFourDay: {
+                        type: 'timeGrid',
+                        duration: { days: 4 },
+                    },
+                }}
+                events={[
+                    { title: "Day One", start: "2025-02-07" },
+                    { title: "Day Two", start: "2025-02-08" },
+                    { title: "Day Three", start: "2025-02-09" },
+                ]}
+                headerToolbar={{
+                    left: "",
+                    center: "title", // Show only the title in the header
+                    right: "",
+                }}
+            />
+        </div>
+    )
+}
--- FrontEndHK/src/pages/SchedulePage.jsx
+++ FrontEndHK/src/pages/SchedulePage.jsx
@@ -1,11 +1,11 @@
 import { Header } from '../components/Header.jsx';
+import { Calendar } from '../components/Calendar.jsx';
 
 export function SchedulePage() {
     return (
         <div>
             <Header />
-            <h1>Welcome to the Schedule Page</h1>
-            <p>This is the Schedule Page of the FrontEndHK application.</p>
+            <Calendar />
         </div>
     )
 }
