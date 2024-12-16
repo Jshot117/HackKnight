bc002503040dc1a770ec2729b6677449defa9ced
Jonnathan Saavedra <jonnjstart@gmail.com>
2024-12-16 02:12:44 -0500

    fixed responsive design for countdown

--- FrontEndHK/src/components/CountDownTimer.jsx
+++ FrontEndHK/src/components/CountDownTimer.jsx
@@ -8,19 +8,19 @@ export function CountDownTimer() {
         date={new Date("2025-03-07T16:00:00")}
         renderer={({ days, hours, minutes, seconds }) => (
           <div className="flex space-x-4">
-            <div className="bg-black text-white text-center rounded-lg p-4 shadow-md">
+            <div className="bg-black text-white text-center rounded-lg p-2 md:p-4 shadow-md">
               <div className="text-3xl font-bold">{days}</div>
               <div className="uppercase text-sm">Days</div>
             </div>
-            <div className="bg-black text-white text-center rounded-lg p-4 shadow-md">
+            <div className="bg-black text-white text-center rounded-lg p-2 md:p-4 shadow-md">
               <div className="text-3xl font-bold">{hours}</div>
               <div className="uppercase text-sm">Hours</div>
             </div>
-            <div className="bg-black text-white text-center rounded-lg p-4 shadow-md">
+            <div className="bg-black text-white text-center rounded-lg p-2 md:p-4 shadow-md">
               <div className="text-3xl font-bold">{minutes}</div>
               <div className="uppercase text-sm">Minutes</div>
             </div>
-            <div className="bg-black text-white text-center rounded-lg p-4 shadow-md">
+            <div className="bg-black text-white text-center rounded-lg p-2 md:p-4 shadow-md">
               <div className="text-3xl font-bold">{seconds}</div>
               <div className="uppercase text-sm">Seconds</div>
             </div>
