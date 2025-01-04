177e08e820c57823f0efb9b7964182977a0d2d27
Jonnathan Saavedra <jonnjstart@gmail.com>
2025-01-04 10:39:46 -0500

    fixed responsive design

--- FrontEndHK/src/components/TeamMember.jsx
+++ FrontEndHK/src/components/TeamMember.jsx
@@ -1,6 +1,6 @@
 export default function TeamMember({ name, role, image, description }) {
   return (
-    <div className="flex flex-col items-center justify-center border-solid border-2 p-8 rounded-2xl w-2/5">
+    <div className="flex flex-col items-center justify-center border-solid border-2 p-8 mx-1 rounded-2xl md:w-2/5">
       <img src={image} alt={name} className="w-48 h-48 rounded-full" />
       <div className="text-xl font-semibold mt-4">{name}</div>
       <div className="text-lg text-gray-500">{role}</div>
