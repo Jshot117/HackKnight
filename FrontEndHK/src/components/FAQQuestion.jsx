225442cdf4462183acc7a46c2521e4786876d4fa
Yonghao <yonghaolin7@gmail.com>
2025-01-04 03:34:27 -0500

    Created FAQ page components. Wrote some questions, need answers

---
+++ FrontEndHK/src/components/FAQQuestion.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useState } from 'react'
+
+export const FAQQuestion = ({question, answer}) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return(
+        <div>
+                <div className={`max-w-4xl w-full mx-auto p-4 rounded-lg shadow-md bg-gray-100 hover:bg-gray-200 ${isOpen ? 'border-2 border-gray-500' : 'border border-gray-300'}`}>
+                    <div className="flex justify-between items-center cursor-pointer font-mono" onClick={() => setIsOpen(!isOpen)}>
+                        <div className="text-black break-words"> {question}</div>
+                        <span>{isOpen ? "▲" : "▼"}</span>
+                    </div>
+                </div>
+                {isOpen && <div className=" text-black max-w-4xl w-full mx-auto p-4 border border-gray-300 rounded-lg shadow-md">{answer}</div>}
+        </div>
+    )
+}
