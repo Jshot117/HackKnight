e3f889acd472b03f3b8bf886be56248a48ca357f
Jshot117 <jonnjstart@gmail.com>
2025-01-07 20:01:57 -0500

    added image to sponsorspage

--- FrontEndHK/src/pages/SponsorsPage.jsx
+++ FrontEndHK/src/pages/SponsorsPage.jsx
@@ -1,11 +1,18 @@
 import { Header } from '../components/Header.jsx';
+import SponsorImage from '../assets/Hackathon Sponsor image.png';
 
 export function SponsorsPage() {
     return (
         <div>
             <Header />
-            <h1>Welcome to the Sponsors Page</h1>
-            <p>This is the Sponsors Page of the FrontEndHK application.</p>
+            <div className='flex flex-col items-center justify-center'>
+                <h1 className="py-2 font-bold text-center text-4xl">Current Sponsors & Partners</h1>
+                <img
+                    src={SponsorImage}
+                    alt="SponsorImage"
+                    className=" p-2">
+                </img>
+            </div>
         </div>
     )
 }
