3f57b8a0133e733cab9c378e5346d8ae805914e0
Jshot117 <jonnjstart@gmail.com>
2025-01-07 22:24:07 -0500

    Fixed typo in register component

--- FrontEndHK/src/components/Register.jsx
+++ FrontEndHK/src/components/Register.jsx
@@ -7,7 +7,7 @@ export function Register() {
             <div className="flex justify-center items-center md:mt-32  ">
                 <div className="md:m-6">
                     <h1 className="text-center text-xl md:text-5xl">
-                        Join an oppurtunity to learn and grow with us!
+                        Join an opportunity to learn and grow with us!
                     </h1>
                 </div>
                 <div className="md:ml-6 border-black border-5 bg-black m-2 p-5 text-white rounded-lg">
