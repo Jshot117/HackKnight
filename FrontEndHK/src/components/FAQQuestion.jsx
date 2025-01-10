99da165d813ad01eeb5c5cb2f1060f45aa96f188
YonghaoL7 <yonghaolin7@gmail.com>
2025-01-09 21:30:45 -0500

    Slight expansion, and added margin

--- FrontEndHK/src/components/FAQQuestion.jsx
+++ FrontEndHK/src/components/FAQQuestion.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
 import { useState } from 'react'
+import {motion, AnimatePresence} from 'framer-motion'
 
 export const FAQQuestion = ({question, answer}) => {
     const [isOpen, setIsOpen] = useState(false);
 
     return(
-        <div>
+        <div className = "mb-4">
                 <div className={`max-w-4xl w-full mx-auto p-4 rounded-lg shadow-md bg-gray-100 hover:bg-gray-200 ${isOpen ? 'border-2 border-gray-500' : 'border border-gray-300'}`}>
                     <div className="flex justify-between items-center cursor-pointer font-mono" onClick={() => setIsOpen(!isOpen)}>
                         <div className="text-black break-normal"> {question}</div>
                         <span>{isOpen ? "▲" : "▼"}</span>
                     </div>
                 </div>
-                {isOpen && <div className=" text-black max-w-2xl md:max-w-4xl w-full mx-auto p-4 border border-gray-300 rounded-lg shadow-md">{answer}</div>}
+                <AnimatePresence>
+                    {isOpen && (
+                        <motion.div
+                            initial={{ maxHeight: 0, opacity: 0 }}
+                            animate={{ maxHeight: 500, opacity: 1 }}
+                            transition={{
+                            maxHeight: { duration: 0.3, ease: 'easeInOut' },
+                            opacity: { duration: 0.3, ease: 'easeInOut' },
+                            }}
+                            className="overflow-hidden bg-gray-100 p-4 rounded-md"
+                        >
+                            <p>{answer}</p>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
         </div>
     )
 }
