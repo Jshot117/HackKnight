00abb30fb288c0e7656dbf81b837c82a54c7a7ef
Jshot117 <jonnjstart@gmail.com>
2025-01-15 16:52:58 -0500

    added more responsive design to count down timer

--- FrontEndHK/src/components/CountDownTimer.jsx
+++ FrontEndHK/src/components/CountDownTimer.jsx
@@ -5,8 +5,15 @@ import "@leenguyen/react-flip-clock-countdown/dist/index.css";
 
 export function CountDownTimer() {
   return (
-    <div className="flex flex-col items-center">
-      <FlipClockCountdown to={new Date("2025-03-07T16:00:00")} />
+    <div className="w-full flex flex-col items-center">
+      <FlipClockCountdown
+        to={new Date("2025-03-07T16:00:00")}
+        digitBlockStyle={{
+          width: '2.2rem', 
+          height: "4rem",
+          fontSize: '2rem'
+        }}
+      />
       <h2 className="text-2xl font-semibold text-blue-700 mt-4">
         UNTIL HACK KNIGHT 2025!
       </h2>
